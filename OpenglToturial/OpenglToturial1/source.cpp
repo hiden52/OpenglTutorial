ecde9b7c0249db589a4d70c96661bfaed4529986
hiden52 <zerorequiem23@gmail.com>
2024-12-31 15:46:48 +0900

    Draw traiangle

--- OpenglToturial/OpenglToturial1/source.cpp
+++ OpenglToturial/OpenglToturial1/source.cpp
@@ -19,11 +19,23 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    glClearColor(0, 0, 0, 1);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glViewport(0, 0, 640, 480);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glColor3f(0.f, 1.f, 0.f);
+        glBegin(GL_TRIANGLES);
+        glVertex3f(0.5f, 0.f, 0.f);
+        glVertex3f(-0.5f, 0.f, 0.f);
+        glVertex3f(0.f, 1.f, 0.f);
+        glEnd();
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
